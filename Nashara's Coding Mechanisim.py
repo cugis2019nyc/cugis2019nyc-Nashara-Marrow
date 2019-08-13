996a568d1c72e28836fe95474409ffe9dbcee34b
Nashara-Marrow <54040002+Nashara-Marrow@users.noreply.github.com>
2019-08-13 13:43:39 -0400

    Add files via upload

---
+++ Nashara's Coding Mechanisim.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("hello world")#helloworld 
+print("1")#numbers
+print("2020")#time
+print("being 17")
+print('5*2=10')
+print('5/2=2.5')      
+print('5-2=3')
+print('5^2=25')
+print('8/9*3~2.6')
+5*2
+5/2
+5-2
+5^2
+8/9*3
+def add(a,b):
+    add = a+b
+    print(add)
+add(4,6)    
+add(9,12)
+def add3(c,d,e):
+    add3 = c+d+e
+add3(1,2,4)
+ print("the sum of"c,d,"and," e ", is =" add3 )
+add3(1,1,1)   
+print('the sum of' a,b, "=" , add)
+add(1,2)
+add3(1,2,3)#day One 
+ def add33(a,b,c)
+     add33(a+b+c):
+         add33(1,1,1)
+add(1,1)
+add(1,1)
+print(add)
+def add(a,b):
+    add= a+b
+    print(add)
+add(1,1)
+def add(a,b):
+    add = a+b
+print(add)
+add(1,2)    
+def 
+
+
+#daY 2 #create funtion multiplication of 3 numbers
+def aqua(a,b,c):
+    aqua = a*b*c
+    print("the product of" ,a,b, "and" ,c, "is" ,aqua)
+
+aqua(2,1,1)
+
+print()
+
+#addition of 3 numbers 
+def water(a,b,c):
+    water = a+b+c
+    print(water)
+#area of Triangle 
+water(1,1,1)
+
+def area(b,h):
+    area = 1/2*b*h
+    print("the area of a triangle with base" ,b, "and height" ,h, "is" ,area)
+    
+area(2,3)   
+
+area(35,12)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
